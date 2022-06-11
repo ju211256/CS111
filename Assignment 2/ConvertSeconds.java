3035cd21de894a41379bb87a9653360c53927f01
ju211256 <106781432+ju211256@users.noreply.github.com>
2022-06-11 19:26:40 -0400

    Update ConvertSeconds.java

--- Assignment 2/ConvertSeconds.java
+++ Assignment 2/ConvertSeconds.java
@@ -3,11 +3,15 @@ public class ConvertSeconds {
         //Declaring Var
         int input = Integer.parseInt(args[0]);
 
-        //Seconds Calculation
+        //Seconds Calculations
         int years = input / 31536000;
+        //1 Year = 31,536,000 Seconds
         int days = (input % 31536000) / 86400;
+        //1 Day = 86,400 Seconds
         int hours = (input % 86400) / 3600;
+        //1 Hour = 3,600 Seconds
         int minutes = (input % 3600) / 60;
+        //1 Minute = 60 Seconds
         int seconds = input % 60;
         System.out.printf("%d years, %d days, %d hours, %d minutes, and %d seconds", years, days, hours, minutes, seconds);
     }
