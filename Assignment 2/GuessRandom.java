210ac25b70f1c923792ba828a6d9e810660193e0
ju211256 <106781432+ju211256@users.noreply.github.com>
2022-06-11 19:21:39 -0400

    Create GuessRandom.java

---
+++ Assignment 2/GuessRandom.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessRandom {
+
+    public static void main(String[] args) {
+        // The setup
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+        int number = random.nextInt(10) + 1;
+        int guess;
+        int subtraction;
+        
+        //Dialogue
+        System.out.println(" I'm thinking of a number from 1 to 10.");
+
+        //Guessing number input
+        System.out.print("Your guess?: ");
+        guess = in.nextInt();
+
+        //Calculating offset
+        subtraction = Math.abs(guess - number);
+
+        System.out.printf("It was %d. You were off by %d.%n", number, subtraction);
+    }
+}
